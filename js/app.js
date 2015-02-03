1b3a83cc076e85fa4ae58dbb8e3ec877f97cc450
ChrisMcKenzie <chris@kbsurfer.com>
2015-02-02 22:25:33 -0800

    added rsvp form

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 var app = angular.module("mandysSite", ["firebase"]);
+var ref = new Firebase("https://mdsite.firebaseio.com/");
+// Returns a random number between min (inclusive) and max (exclusive)
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 app.controller("guestbook", ["$scope", "$firebase", function($scope, $firebase) {
-    console.log("asdasd");
+    // console.log("asdasd");
     $scope.sig = {
       name: null,
       email: null,
       message: null
     };
-    var ref = new Firebase("https://mdsite.firebaseio.com/");
     // create an AngularFire reference to the data
     var sync = $firebase(ref.child("guestbook"));
     // download the data into a local object
@@ -19,4 +23,26 @@ app.controller("guestbook", ["$scope", "$firebase", function($scope, $firebase) 
       $scope.signatures.$add($scope.sig);
     }
   }
-]);
+]).controller("rsvp", ["$scope", "$firebase", function($scope, $firebase){
+  var one = getRandomArbitrary(0, 10);
+  var two = getRandomArbitrary(0, 10);
+  var answer = one + two
+  $scope.question = "what is " + one + " + " + two;
+  $scope.rsvp = {}
+    // create an AngularFire reference to the data
+  var sync = $firebase(ref.child("rsvps"));
+  // download the data into a local object
+  var rsvps = sync.$asArray();
+
+  $scope.submit = function(){
+    if($scope.answer == answer || ($scope.question == "Are you a robot?" && $scope.answer === "no")){
+      $scope.rsvp.date = Date.now();
+      rsvps.$add($scope.rsvp);
+      $scope.message = "Rsvp Successfully Sent..."
+    } else if($scope.question == "Are you a robot?" && $scope.answer !== "no") {
+      $scope.message = "We don't serve your kind! droids have to wait outside"
+    } else {
+      $scope.question = "Are you a robot?"
+    }
+  }
+}])
