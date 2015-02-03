1406f772b42612c5aea7b7cd36849edc7f4ea565
ChrisMcKenzie <chris@kbsurfer.com>
2015-02-02 21:57:50 -0800

    added ability to submit guestbook message. FUCK YEAH!!

--- js/app.js
+++ js/app.js
@@ -2,11 +2,21 @@ var app = angular.module("mandysSite", ["firebase"]);
 
 app.controller("guestbook", ["$scope", "$firebase", function($scope, $firebase) {
     console.log("asdasd");
+    $scope.sig = {
+      name: null,
+      email: null,
+      message: null
+    };
     var ref = new Firebase("https://mdsite.firebaseio.com/");
     // create an AngularFire reference to the data
     var sync = $firebase(ref.child("guestbook"));
     // download the data into a local object
     $scope.signatures = sync.$asArray();
-    console.log($scope.signatures);
+    // console.log($scope.signatures);
+
+    $scope.submit = function(){
+      $scope.sig.date = Date.now();
+      $scope.signatures.$add($scope.sig);
+    }
   }
 ]);
